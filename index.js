99be13cde92222423226302c4643e9fc6f37662a
Rohitkrranjan <ranjanrohitkumar498@gmail.com>
2023-10-25 09:20:24 +0530

    Core modules

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+/*
 const app = require('./app')
 
 console.log(app)
+*/
 
 // let a=20;
 // let b=30;
 // let c=40;
 // console.log(a+b+c)
 
-const arr =[1,2,3,4,5,6,7,8]
+// const arr =[1,2,3,4,5,6,7,8]
 
-let result = arr.filter((item)=>{
-    return item > 3
-})
+// let result = arr.filter((item)=>{
+//     return item > 3
+// })
 
-console.log(result)
+// console.log(result)
+
+const fs = require('fs')
+
+fs.writeFileSync("hello.txt","learn node js from step by step")
+console.log("==>",__dirname)
+console.log("file_name",__filename)
