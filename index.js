ae6b2bcc0630875ce1af470183e22d00c2b1d7b1
Rohitkrranjan <ranjanrohitkumar498@gmail.com>
2023-10-25 09:04:31 +0530

    basic fundamental of javaScript

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("sub")
+const app = require('./app')
+
+console.log(app)
+
+// let a=20;
+// let b=30;
+// let c=40;
+// console.log(a+b+c)
+
+const arr =[1,2,3,4,5,6,7,8]
+
+let result = arr.filter((item)=>{
+    return item > 3
+})
+
+console.log(result)
