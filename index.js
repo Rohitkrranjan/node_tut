a34f35d17293ddc128eab36292ca5b9fe4c59077
Rohitkrranjan <ranjanrohitkumar498@gmail.com>
2023-10-26 21:14:36 +0530

    Render html

--- index.js
+++ index.js
@@ -165,17 +165,24 @@ console.log(a+b);
 const express = require('express');
 const app = express();
 
-app.get('',(req , res)=>{
-    console.log("data sent by browser ===>>",req.query.name)
-    res.send('Welcome ,'+req.query.name);
-});
+// app.get('',(req , res)=>{
+//     console.log("data sent by browser ===>>",req.query.name)
+//     res.send('Welcome ,'+req.query.name);
+// });
+
+app.get('',(req,resp)=>{
+    resp.send(`<h1>welcome , to Home page </h1> <a href="/about">Go to About page  </a> `);
+
+})
 
 app.get('/about',(req , res)=>{
     res.send("Hello , this is about page ");
 });
 
 app.get('/help',(req,res)=>{
-    res.send("Hello , this is help page");
+    res.send(`<input type="text" placeholder="User name" /> 
+    <button>Clike Me </button>
+    <a href="/">Go to Home  page  </a> `);
 });
 
-app.listen(4400);
+app.listen(4300);
